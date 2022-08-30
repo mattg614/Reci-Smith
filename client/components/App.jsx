f893deb0f1268a9932031415a4cfb7fd5fb32512
Matthew Garza <mattg614@gmail.com>
2022-08-30 12:22:04 -0400

    HelloWorldReactApp Up

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const App = props => {
+  return (
+    <div>
+      <main>
+        <h1>HelloWorldthisisReciSmith</h1>
+      </main>
+    </div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack');
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './client/index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      //TODO add in css and scss later
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+};
